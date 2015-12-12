f32433190939008abadbcbd24cab70fd5aaa5258
Travisws <travisws@me.com>
2015-12-11 23:14:53 -0700

    More Updates

--- Golang/src/github.com/travisws/main.go
+++ Golang/src/github.com/travisws/main.go
@@ -1,27 +1,28 @@
 package main
 
 import (
-	"net"
-	"fmt"
 	"bufio"
+	"fmt"
+	"net"
 	"strings"
+    "os/exec"
 )
 
 //TODO Work on rest interface.
 func main() {
 	fmt.Println("Launching server...")
 	// listen on all interfaces
-	ln, _ := net.Listen("tcp", ":8081")   // accept
-	conn, _ := ln.Accept()   // run loop forever (or until ctrl-c)
+	ln, _ := net.Listen("tcp", ":8081") // accept
+	conn, _ := ln.Accept()              // run loop forever (or until ctrl-c)
 	on := "on\n"
 	message, _ := bufio.NewReader(conn).ReadString('\n')
 
 	for {
 		if message == on {
-			newmessage := strings.ToUpper(message)     // send new string back to client
+			newmessage := strings.ToUpper(message) // send new string back to client
 			conn.Write([]byte(newmessage + "\n"))
 			led()
-		}else {
+		} else {
 			conn.Write([]byte("Did not work\n"))
 		}
 
@@ -31,5 +32,10 @@ func main() {
 
 //Turns LED on and off
 func led() {
+	//exec.Command("led.py").Run()
+	c := exec.Command("led.sh")
 
+	if err := c.Run(); err != nil {
+		fmt.Println("Error: ", err)
+	}
 }
