c64ef0b2675b67f7f41b9844342f05c49a10019c
Travis Woodworth-Smith <travisws@me.com>
2016-01-10 20:26:05 -0700

    Switch languages to java and made the android app. At the moment that is working Server side and app side. Need to update README.md

---
+++ GarageDoorOpenerAndroidApp/app/src/main/java/garagedooropener/travisws/com/garagedooropener/MainActivity.java
@@ -0,0 +1,95 @@
+package garagedooropener.travisws.com.garagedooropener;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int SERVERPORT = 8081;
+    private static final String SERVER_IP = "192.168.0.31";
+    private Socket socket;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        new Thread(new ClientThread()).start();
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        new Thread(new ClientThread()).start();
+    }
+
+    public void onClick(View view) {
+        try {
+           /* EditText et = (EditText) findViewById(R.id.EditText01);
+            String str = et.getText().toString();*/
+            String str = "on";
+            PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+            out.println(str);
+            Snackbar.make(view, "Sent", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+            //out.println("off");
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    class ClientThread implements Runnable {
+        @Override
+        public void run() {
+            try {
+                InetAddress serverAddr = InetAddress.getByName(SERVER_IP);
+                socket = new Socket(serverAddr, SERVERPORT);
+            } catch (UnknownHostException e1) {
+                e1.printStackTrace();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+
+            }
+        }
+    }
+}
